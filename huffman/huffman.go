a5ecb581125b7651ec5a44eeb01d33e09f50f4e2
Fionn Bracken <fionnbracken@Fionns-mbp.local>
2022-06-05 17:38:33 +0100

    counter and sorting implemented

--- huffman/huffman.go
+++ huffman/huffman.go
@@ -1,24 +1,54 @@
 package main
 
-
+import "sort"
 
 func Encode(s string) string {
-
-
-
     return "1000011101001000110010011101100111001001000111110010011111011111100010001111110100111001001011111011101000111111001";
 }
 
-func Counter(s string) map[rune]int {
-    counter := make(map[rune]int);
-
+func Counter(s string) map[string]int {
+    counter := make(map[string]int);
     for _, val := range s {
-        _, ok := counter[val];
+        strVal := string(val);
+        _, ok := counter[strVal];
         if !ok {
-            counter[val] = 1;
+            counter[strVal] = 1;
         } else {
-            counter[val] += 1;
+            counter[strVal] += 1;
         }
     }
     return counter;
 }
+
+type Node struct {
+    right *Node
+    left *Node
+}
+
+type LeafNode struct {
+    Node
+    value string
+}
+
+type Pair struct {
+    Key string
+    Value int
+}
+
+type PairList []Pair
+
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p PairList) Less(i, j int) bool { return p[i].Value < p[i].Value }
+
+func SortCounter(c map[string]int) PairList {
+    p := make(PairList, len(c));
+    i := 0;
+    for k, v := range c {
+        p[i] = Pair{k, v};
+        i++;
+    }
+    sort.Sort(p);
+    return p;
+}
+
--- huffman/huffman_test.go
+++ huffman/huffman_test.go
@@ -8,14 +8,14 @@ import (
 
 func TestCounter(t *testing.T) {
     inputs := []string{"AAABBC", "AABBBCB", "BB", "CBCBAAA"};
-    expected := [4]map[rune]int {
-        {'A':3, 'B':2, 'C':1},
-        {'A':2, 'B':4, 'C':1},
-        {'B':2},
-        {'A':3, 'B':2, 'C':2},
+    expected := [4]map[string]int {
+        {"A":3, "B":2, "C":1},
+        {"A":2, "B":4, "C":1},
+        {"B":2},
+        {"A":3, "B":2, "C":2},
     };
 
-    var outputs [4]map[rune]int;
+    var outputs [4]map[string]int;
     for i, val := range inputs {
         outputs[i] = Counter(val);
     }
@@ -29,6 +29,22 @@ func TestCounter(t *testing.T) {
     fmt.Println("Counter tests passed");
 }
 
+func TestSortCounter(t *testing.T) {
+    input := map[string]int {"A":3, "B":2, "C":1};
+    expected := PairList{
+        {"A", 3},
+        {"B", 2},
+        {"C", 1},
+    };
+    output := SortCounter(input)
+
+    if !reflect.DeepEqual(output, expected) {
+        t.Fatalf("Output: %v\nExpected: %v", output, expected);
+    }
+
+    fmt.Println("SortCounter tests passed");
+}
+
 func TestHuffmanEncoding(t *testing.T) {
     input := "A_DEAD_DAD_CEDED_A_BAD_BABE_A_BEADED_ABACA_BED";
     expected := "1000011101001000110010011101100111001001000111110010011111011111100010001111110100111001001011111011101000111111001";
@@ -40,6 +56,6 @@ func TestHuffmanEncoding(t *testing.T) {
     }
 
     fmt.Println("Encode tests passed");
-    fmt.Printf("# Bits in Input: %d \n# Bits in Output: %d\n",
+    fmt.Printf("# Bits in Input: %d \t# Bits in Output: %d\n\n",
                 len(input)*8, len(output));
 }
